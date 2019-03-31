a5496490118c192e52711134d9ad800128a494be
Маслова Екатерина <python.ea@gmail.com>
2019-03-31 22:31:31 +0300

    Homework1 and homework2 for C#

---
+++ Array/Array/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press \"Ctrl+C\" for exit");
+
+            //По ТЗ известно, что массив будет двумерный
+            int[,] array_1 = new int[2,2];
+            int[,] array_2 = new int[2,2];
+
+            //Заполнение матрицы
+            Console.WriteLine("Введите элементы первого массива:");
+            fillArray(array_1);
+
+            Console.WriteLine("Введите элементы второго массива:");
+            fillArray(array_2);
+
+            //Сложение двух массивов
+            int[,] array_sum = new int[2, 2];
+            for (int i = 0; i < array_1.GetLength(0); i++)
+            {
+                for (int j = 0; j < array_2.GetLength(1); j++)
+                {
+                    array_sum[i, j] = array_1[i, j] + array_2[i, j];
+                }
+            }
+
+            //Вывод матрицы
+            //Console.WriteLine("Массив №1:");
+            //printArray(array_1);
+
+            //Console.WriteLine("Массив №2:");
+            //printArray(array_2);
+
+            Console.WriteLine("Суммарный массив:");
+            printArray(array_sum);
+
+            Console.ReadLine();
+
+        }
+
+        //Метод вывода массива на экран
+        static void printArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(" array[" + i + "," + j + "]: " + array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Метод ввода массива с клавиатуры
+        static void fillArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("array[" + i + "," + j + "]: ");
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine('\n');
+        }
+
+    }
+
+}
---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I am first calc by Kate. You can add(+), subtraction(-), multiply(*) and divide(/) a numbers. Press \"Ctrl+C\" for exit.");
+
+            while (true)
+            {
+
+                Console.Write("What kind of operation do you want? Write operator: ");
+                string op = Console.ReadLine();
+
+                Console.Write("Write first arg: ");
+                string line = Console.ReadLine();
+                int arg1 = 0;
+
+                bool ok = int.TryParse(line, out arg1);
+                if (!ok) {
+                    Console.WriteLine("Error! Write number!");
+                    continue;
+                }              
+
+                Console.Write("Write second arg: ");
+                line = Console.ReadLine();
+                int arg2 = int.Parse(line);
+
+                double result = 0;
+
+                switch (op)
+                {
+                    case "+":
+                        result = add(arg1, arg2);
+                        break;
+                    case "-":
+                        result = sub(arg1, arg2);
+                        break;
+                    case "*":
+                        result = mult(arg1, arg2);
+                        break;
+                    case "/":
+                        result = div(arg1, arg2);
+                        break;
+                    default:
+                        Console.WriteLine("Error! Write correct operation!");
+                        break;
+                }
+
+                Console.WriteLine("Result: {0}", result);
+
+                Console.Write("Do you want to continue? Press any key to continue or 'N' to exit: ");
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.N:
+                    case ConsoleKey.Escape:
+                        return;
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+
+        static double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double mult(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+
+}
